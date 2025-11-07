984321124a4a53ddd8e3b1546e656d2bd60c348c
milescor000 <corey.miles18@gmail.com>
2025-11-06 23:16:57 -0700

    Line Following 1.2

--- Rover_Drive_Code.c
+++ Rover_Drive_Code.c
@@ -15,7 +15,101 @@
 #pragma config OSCIOFNC = OFF
 
 // turn of secondary oscillator
-#pragma config SOSCSRC = DIG 
+#pragma config SOSCSRC = DIG
+
+void line_following(void){
+    
+    // line detection
+    if (ADC1BUF13 < 1000){
+            
+        // left QRD activated
+        if (ADC1BUF15 < 1000){
+                
+        // right motor normal
+        OC1RS = 78;
+        OC1R = 39;
+                
+        // left motor slow
+        OC2RS = 156;
+        OC2R = 78;
+                
+        }
+            
+        // right QRD activated
+        else if (ADC1BUF4 < 1000){
+                
+            // left motor normal
+            OC2RS = 78;
+            OC2R = 39;
+                
+            // right motor slow
+            OC1RS = 156;
+            OC1R = 78;
+                
+        }
+            
+        // only middle QRD activated
+        else{
+                
+            // both motors equal
+            OC1RS = 78;
+            OC1R = 39;
+            OC2RS = 78;
+            OC2R = 39;
+            
+        }
+               
+    }
+    
+}
+
+void config_ad(void){
+    
+    _ADON = 0;          // AD1CON1<15> -- Turn off A/D during config
+    
+    // Clear all A/D registers
+    AD1CON1 = 0; 
+    AD1CON2 = 0; 
+    AD1CON3 = 0; 
+    AD1CON5 = 0; 
+    AD1CSSL = 0; 
+    AD1CSSH = 0; 
+    
+    // AD1CON1 register
+    _ADSIDL = 0;    // AD1CON1<13> -- A/D continues while in idle mode
+    _MODE12 = 1;    // AD1CON1<10> -- 12-bit A/D operation
+    _FORM = 0;      // AD1CON1<9:8> -- Unsigned integer output
+    _SSRC = 7;      // AD1CON1<7:4> -- Auto conversion (internal counter)
+    _ASAM = 1;      // AD1CON1<2> -- Auto sampling
+
+    // AD1CON2 register
+    _PVCFG = 0;     // AD1CON2<15:14> -- Use VDD as positive ref voltage
+    _NVCFG = 0;     // AD1CON2<13> -- Use VSS as negative ref voltage
+    _BUFREGEN = 1;  // AD1CON2<11> -- Result appears in buffer
+                    // location corresponding to channel, e.g., AN12
+                    // results appear in ADC1BUF12
+    _CSCNA = 1;     // AD1CON2<10> -- Scans inputs specified in AD1CSSx
+                    // registers
+    _SMPI = 2;    // AD1CON2<6:2> -- Results sent to buffer after n conversion
+                    // For example, if you are sampling 4 channels, you
+                    // should have _SMPI = 3;
+    _ALTS = 0;      // AD1CON2<0> -- Sample MUXA only
+
+    // AD1CON3 register -- Change _SAMC and _ADCS based on your
+    // selection of oscillator and postscaling
+    _ADRC = 0;      // AD1CON3<15> -- Use system clock
+    _SAMC = 1;      // AD1CON3<12:8> -- Auto sample every A/D period TAD
+    _ADCS = 0;      // AD1CON3<7:0> -- A/D period TAD = TCY
+    
+    // AD1CHS register
+    _CH0NA = 0;     // AD1CHS<7:5> -- Measure voltages relative to VSS
+    _CSS4 = 1;
+    _CSS13 = 1;
+    _CSS15 = 1;
+    
+    _ADON = 1;      // AD1CON1<15> -- Turn on A/D
+    
+}
 
 int main(void){
     
@@ -59,97 +153,4 @@ int main(void){
     
 return 0;
 
-}
-
-void line_following(void){
-    
-    // line detection
-    if (ADC1BUF13 <= 1000){
-            
-        // left QRD activated
-        if (ADC1BUF15 <= 1000){
-                
-        // right motor normal
-        OC1RS = 78;
-        OC1R = 39;
-                
-        // left motor slow
-        OC2RS = 39;
-        OC2R = 20;
-                
-        }
-            
-        // right QRD activated
-        else if (ADC1BUF4 <= 1000){
-                
-            // left motor normal
-            OC2RS = 78;
-            OC2R = 39;
-                
-            // right motor slow
-            OC1RS = 39;
-            OC1R = 20;
-                
-        }
-            
-        // only middle QRD activated
-        else{
-                
-            // set PWM equal
-            OC1RS = 78;
-            OC1R = 39;
-            OC2RS = 78;
-            OC2R = 39;
-            
-        }
-               
-    }
-}
-
-void config_ad(void){
-    
-    _ADON = 0;          // AD1CON1<15> -- Turn off A/D during config
-    
-    // Clear all A/D registers
-    AD1CON1 = 0; 
-    AD1CON2 = 0; 
-    AD1CON3 = 0; 
-    AD1CON5 = 0; 
-    AD1CSSL = 0; 
-    AD1CSSH = 0; 
-    
-    // AD1CON1 register
-    _ADSIDL = 0;    // AD1CON1<13> -- A/D continues while in idle mode
-    _MODE12 = 1;    // AD1CON1<10> -- 12-bit A/D operation
-    _FORM = 0;      // AD1CON1<9:8> -- Unsigned integer output
-    _SSRC = 7;      // AD1CON1<7:4> -- Auto conversion (internal counter)
-    _ASAM = 1;      // AD1CON1<2> -- Auto sampling
-
-    // AD1CON2 register
-    _PVCFG = 0;     // AD1CON2<15:14> -- Use VDD as positive ref voltage
-    _NVCFG = 0;     // AD1CON2<13> -- Use VSS as negative ref voltage
-    _BUFREGEN = 1;  // AD1CON2<11> -- Result appears in buffer
-                    // location corresponding to channel, e.g., AN12
-                    // results appear in ADC1BUF12
-    _CSCNA = 1;     // AD1CON2<10> -- Scans inputs specified in AD1CSSx
-                    // registers
-    _SMPI = 2;    // AD1CON2<6:2> -- Results sent to buffer after n conversion
-                    // For example, if you are sampling 4 channels, you
-                    // should have _SMPI = 3;
-    _ALTS = 0;      // AD1CON2<0> -- Sample MUXA only
-
-    // AD1CON3 register -- Change _SAMC and _ADCS based on your
-    // selection of oscillator and postscaling
-    _ADRC = 0;      // AD1CON3<15> -- Use system clock
-    _SAMC = 1;      // AD1CON3<12:8> -- Auto sample every A/D period TAD
-    _ADCS = 0;      // AD1CON3<7:0> -- A/D period TAD = TCY
-    
-    // AD1CHS register
-    _CH0NA = 0;     // AD1CHS<7:5> -- Measure voltages relative to VSS
-    _CSS3 = 1;
-    _CSS4 = 1;
-    _CSS13 = 1;
-    
-    _ADON = 1;      // AD1CON1<15> -- Turn on A/D
-    
 }
